cb115ab38f4c5557e6c7264a96f74d4a6ab5f181
Srijan Nayak <srijannayak0@gmail.com>
2019-07-21 14:53:56 +0530

    Add event listeners to grid size inputs

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log("Everything works!");
+function handleSizeChange(event) {
+	let rows = document.getElementById("grid-rows-input").value;
+	let columns = document.getElementById("grid-columns-input").value;
+	console.log(`Rows: ${rows}, Columns: ${columns}`);
+}
+
+let rowController = document.getElementById("grid-rows-input");
+let columnController = document.getElementById("grid-columns-input");
+
+rowController.addEventListener("input", handleSizeChange);
+columnController.addEventListener("input", handleSizeChange);
