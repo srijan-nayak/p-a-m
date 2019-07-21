74ebd136d58de375c03b6385c7549aada18df1d7
Srijan Nayak <srijannayak0@gmail.com>
2019-07-21 19:50:33 +0530

    Make a grid according to grid size input

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 function handleSizeChange(event) {
 	let rows = document.getElementById("grid-rows-input").value;
 	let columns = document.getElementById("grid-columns-input").value;
+	makeGrid(rows, columns);
 	console.log(`Rows: ${rows}, Columns: ${columns}`);
+}
+
+function makeGrid(rows, columns) {
+	let grid = document.getElementById("grid");
+	grid.innerHTML = "";
+	for (let i = 0; i < rows; i++) {
+		let newRow = document.createElement("tr");
+		for (let j = 0; j < columns; j++) {
+			let newCell = document.createElement("td");
+			newCell.classList.add("grid-cell");
+			newRow.appendChild(newCell);
+		}
+		grid.appendChild(newRow);
+	}
 }
 
 let rowController = document.getElementById("grid-rows-input");
